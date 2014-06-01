a0b74d05dad0210f5703013a755692cca05f56d3
ypxu <nelson0235@gmail.com>
2014-06-01 11:45:21 -0700

    fix post photo absolute url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	# return abosolute url for a post's photo
+	def post_photo_url(photo, style = :medium)
+		"#{request.protocol}#{request.host_with_port}#{photo.url(style)}"
+	end
 end
